30382117c73887152e1ec050bd607771b2e36363
Ishaan Joshi <joshi.ishaan.2001@gmail.com>
2023-06-29 16:47:15 +0530

    Taking States from Data

--- src/Components/ChooseCity/Index.jsx
+++ src/Components/ChooseCity/Index.jsx
@@ -1,12 +1,21 @@
 import React from 'react'
+import cities from '../../Data/IndianCities.json'
 
 const ChooseCity = () => {
   return (
     <div>
         <div className="stateWrap">
-            <select className="stateMenu">  
-                <option value="Hello World">Hello World</option>
-            </select>
+            <select className="stateMenu">
+				{
+					cities && cities.length > 0 && cities.map( (city) => {
+						return (
+							<option key={city.lat + city.lng}>
+								{city.city}, {city.admin_name}
+							</option>
+						)
+					} )
+				}
+			</select>
         </div>
     </div>
   )
