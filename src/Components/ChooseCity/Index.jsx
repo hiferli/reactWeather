613635322ca2b05514ead53454f8bf441ee0e1cf
Ishaan Joshi <joshi.ishaan.2001@gmail.com>
2023-06-29 17:37:41 +0530

    Using Context to Store States Globally

--- src/Components/ChooseCity/Index.jsx
+++ src/Components/ChooseCity/Index.jsx
@@ -1,11 +1,30 @@
 import React from 'react'
 import cities from '../../Data/IndianCities.json'
 
+import { UseWeatherAppContext } from '../../Context/Context'
+
 const ChooseCity = () => {
+	console.log(`UseWeatherAppContext: ` , UseWeatherAppContext())
+
+	const {state: {city} , dispatch} = UseWeatherAppContext();
+	// console.log(city);
+
+	const handleChange = (e) => {
+		const selectedCity = cities.filter((city) => {
+			return city.city === e.target.value.split(',')[0];			;
+		})[0];
+
+		// console.log(selectedCity);
+		dispatch({
+			type: 'SET_CITY',
+			payload: selectedCity
+		})
+	}
+
   return (
     <div>
         <div className="stateWrap">
-            <select className="stateMenu">
+            <select className="stateMenu" defaultValue={city} onChange={handleChange}>
 				{
 					cities && cities.length > 0 && cities.map( (city) => {
 						return (
