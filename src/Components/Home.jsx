3a84c6bca67576e1862894219ee10779674c7a24
Ishaan Joshi <joshi.ishaan.2001@gmail.com>
2023-06-29 21:31:50 +0530

    Adding SideBar to Home

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,16 +1,19 @@
 import React from 'react'
 import ChooseCity from './ChooseCity/Index'
 import WeekInfo from './Weekly/WeekInfo'
+import Humidity from './Humidity/Humidity'
+import Sidebar from './SideBar/Sidebar'
 
 const Home = () => {
   return (
     <>
         <div className='homeWrap'>
             <div className="weatherSection">
-                <div className="rightSegment">
-                    <ChooseCity />
-                    <WeekInfo /> <br />
-                    3. Humidity
+                <Sidebar />
+                <div className="rightSide">
+                    <ChooseCity /> 
+                    <WeekInfo /> 
+                    <Humidity />
                 </div>
             </div>
         </div>
